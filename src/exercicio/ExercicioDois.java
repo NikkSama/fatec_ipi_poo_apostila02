61869db9e3fb459456a3cc6bd4f877be7972e9c6
NikkSama <audrey.sampaiobrito@gmail.com>
2021-08-24 14:30:28 -0300

    Exercicio 2 da apostila 02 - ternario

--- src/exercicio/ExercicioDois.java
+++ src/exercicio/ExercicioDois.java
@@ -8,11 +8,17 @@ public class ExercicioDois {
         idade = Integer.parseInt(JOptionPane.showInputDialog("Qual é a sua idade? "));
 
         //if/else
-        if(idade >= 18) {
-            JOptionPane.showMessageDialog(null, "Maior de idade.");
-        } else{
-            JOptionPane.showMessageDialog(null, "Menor de idade.");
-        }
+//        if(idade >= 18) {
+//            JOptionPane.showMessageDialog(null, "Maior de idade.");
+//        } else{
+//            JOptionPane.showMessageDialog(null, "Menor de idade.");
+//        }
+
+        //ternario
+        String resultado;
+        resultado = idade >= 18 ? "Maior de idade. " : "Menor de idade. ";
+        JOptionPane.showMessageDialog(null, resultado);
+
     }
 }
 
